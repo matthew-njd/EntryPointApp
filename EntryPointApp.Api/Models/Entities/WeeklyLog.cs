fd3353d49a668d7d54fe7859cd718ab6339f967d
Matthew D'Agostino <mnjdagostino@gmail.com>
2025-07-11 19:20:24 -0400

    WeeklyLog Entity

---
+++ EntryPointApp.Api/Models/Entities/WeeklyLog.cs
@@ -0,0 +1,23 @@
+namespace EntryPointApp.Api.Models.Entities
+{
+    public class WeeklyLog
+    {
+        public int Id { get; set; }
+
+        public required int UserId { get; set; }
+
+        public required DateTime Date { get; set; }
+
+        public decimal Hours { get; set; }
+
+        public decimal Mileage { get; set; }
+
+        public decimal TollCharge { get; set; }
+
+        public decimal ParkingFee { get; set; }
+
+        public decimal OtherCharges { get; set; }
+
+        public string Comment { get; set; } = string.Empty;
+    }
+}
