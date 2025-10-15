f36d51918d5bb476924578865cfae3022fffd57a
Matthew D'Agostino <mnjdagostino@gmail.com>
2025-10-14 20:29:03 -0400

    middleware clean up

--- EntryPointApp.Api/Middleware/SecurityHeadersMiddleware.cs
+++ EntryPointApp.Api/Middleware/SecurityHeadersMiddleware.cs
@@ -17,18 +17,14 @@ namespace EntryPointApp.Api.Middleware
             var headers = context.Response.Headers;
 
             headers.Append("X-Frame-Options", "DENY");
-
             headers.Append("X-Content-Type-Options", "nosniff");
-
             headers.Append("X-XSS-Protection", "1; mode=block");
-
             headers.Append("Referrer-Policy", "strict-origin-when-cross-origin");
 
             headers.Remove("Server");
             headers.Remove("X-Powered-By");
 
             headers.Append("Content-Security-Policy", "default-src 'none'; frame-ancestors 'none';");
-
             headers.Append("Permissions-Policy", "geolocation=(), microphone=(), camera=()");
         }
     }
