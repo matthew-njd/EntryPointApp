71bb7f2730641532bfa32f9831f2a4d5bcb15cd2
Matthew D'Agostino <mnjdagostino@gmail.com>
2025-11-20 12:06:35 -0500

    Fixed class naming

--- EntryPointApp.Api/Models/Dtos/WeeklyLog/WeeklyLogResult.cs
+++ EntryPointApp.Api/Models/Dtos/WeeklyLog/WeeklyLogResult.cs
@@ -2,19 +2,19 @@ using EntryPointApp.Api.Models.Dtos.Common;
 
 namespace EntryPointApp.Api.Models.Dtos.WeeklyLog
 {
-    public class BaseWeeklylogResult
+    public class BaseWeeklyLogResult
     {
         public bool Success { get; set; }
         public string Message { get; set; } = string.Empty;
         public List<string> Errors { get; set; } = [];
     }
 
-    public class WeeklyLogResult : BaseWeeklylogResult
+    public class WeeklyLogResult : BaseWeeklyLogResult
     {
         public WeeklyLogResponse? Data { get; set; }
     }
 
-    public class WeeklyLogListResult : BaseWeeklylogResult
+    public class WeeklyLogListResult : BaseWeeklyLogResult
     {
         public PagedResult<WeeklyLogResponse>? Data { get; set; }
     }
