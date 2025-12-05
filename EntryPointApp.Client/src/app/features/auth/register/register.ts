1da74cfe28169fc37224e4224bfc331913e93739
Matthew D'Agostino <mnjdagostino@gmail.com>
2025-12-05 15:29:08 -0500

    Added more fields to register form and basic form group

--- EntryPointApp.Client/src/app/features/auth/register/register.ts
+++ EntryPointApp.Client/src/app/features/auth/register/register.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  private fb = inject(FormBuilder);
 
+  registerForm: FormGroup;
+  errorMessage = '';
+
+  constructor() {
+    this.registerForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit(): void {
+    this.errorMessage = '';
+
+    const { firstName, lastName, email, password } = this.registerForm.value;
+    console.log(firstName, lastName, email, password);
+  }
 }
