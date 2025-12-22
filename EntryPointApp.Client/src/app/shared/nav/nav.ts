8eaeb79804e58f53332a8b0d843b8511915e143f
Matthew D'Agostino <mnjdagostino@gmail.com>
2025-12-22 11:32:23 -0500

    Added toast to logout

--- EntryPointApp.Client/src/app/shared/nav/nav.ts
+++ EntryPointApp.Client/src/app/shared/nav/nav.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../core/services/auth.service';
 import { Router } from '@angular/router';
+import { ToastService } from '../../core/services/toast.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,10 +11,12 @@ import { Router } from '@angular/router';
 })
 export class Nav {
   private authService = inject(AuthService);
+  private toastService = inject(ToastService);
   private router = inject(Router);
 
   onLogout(): void {
     this.authService.logout();
+    this.toastService.success("You've successfully logged out!");
     this.router.navigate(['/login']);
   }
 }
