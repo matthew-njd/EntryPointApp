9d08392ddd79130710d3cab08bd9066dfeeb7f40
Matthew D'Agostino <mnjdagostino@gmail.com>
2025-12-22 11:23:54 -0500

    Fixed footer spacing on Dashboad/Dailylog pages

--- EntryPointApp.Client/src/app/app.ts
+++ EntryPointApp.Client/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Footer } from './shared/footer/footer';
 import { ToastComponent } from './shared/toast/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, ToastComponent],
+  imports: [RouterOutlet, ToastComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
